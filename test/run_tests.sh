85359e9f88624d55e1cc76e617abc2cc599aa8bf
Jordan Carlin <jordanmcarlin@gmail.com>
2024-11-27 23:36:37 -0800

    Test precompiled binary

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -47,73 +47,20 @@ function finish_suite {
     fail=0
 }
 
-SAILLIBDIR="$DIR/../../lib/"
-
 cd $RISCVDIR
 
-# Do 'make clean' to avoid cross-arch pollution.
-make clean
 
-printf "Building 32-bit RISCV specification...\n"
+printf "Running 64-bit RISCV tests...\n"
 
-if ARCH=RV32 make c_emulator/riscv_sim_RV32;
-then
-    green "Building 32-bit RISCV C emulator" "ok"
-else
-    red "Building 32-bit RISCV C emulator" "fail"
-fi
-for test in $DIR/riscv-tests/rv32*.elf; do
-    if timeout 5 $RISCVDIR/c_emulator/riscv_sim_RV32 -p $test > ${test%.elf}.cout 2>&1 && grep -q SUCCESS ${test%.elf}.cout
-    then
-	green "C-32 $(basename $test)" "ok"
-    else
-	red "C-32 $(basename $test)" "fail"
-    fi
-done
-finish_suite "32-bit RISCV C tests"
-
-# Do 'make clean' to avoid cross-arch pollution.
-make clean
-
-printf "Building 64-bit RISCV specification...\n"
-
-if make c_emulator/riscv_sim_RV64;
-then
-    green "Building 64-bit RISCV C emulator" "ok"
-else
-    red "Building 64-bit RISCV C emulator" "fail"
-fi
 for test in $DIR/riscv-tests/rv64*.elf; do
-    if timeout 5 $RISCVDIR/c_emulator/riscv_sim_RV64 -p $test > ${test%.elf}.cout 2>&1 && grep -q SUCCESS ${test%.elf}.cout
-    then
-	green "C-64 $(basename $test)" "ok"
-    else
-	red "C-64 $(basename $test)" "fail"
-    fi
+    riscv_sim_RV64 -p $test #> ${test%.elf}.cout 2>&1 && grep -q SUCCESS ${test%.elf}.cout
+    # then
+	# green "C-64 $(basename $test)" "ok"
+    # else
+	# red "C-64 $(basename $test)" "fail"
+    # fi
 done
 finish_suite "64-bit RISCV C tests"
-
-# Do 'make clean' to avoid cross-arch pollution.
-make clean
-
-if ARCH=RV32 make c_emulator/riscv_rvfi_RV32;
-then
-    green "Building 32-bit RISCV RVFI C emulator" "ok"
-else
-    red "Building 32-bit RISCV RVFI C emulator" "fail"
-fi
-finish_suite "32-bit RISCV RVFI C tests"
-
-# Do 'make clean' to avoid cross-arch pollution.
-make clean
-
-if ARCH=RV64 make c_emulator/riscv_rvfi_RV64;
-then
-    green "Building 64-bit RISCV RVFI C emulator" "ok"
-else
-    red "Building 64-bit RISCV RVFI C emulator" "fail"
-fi
-finish_suite "64-bit RISCV RVFI C tests"
 
 printf "Passed ${all_pass} out of $(( all_pass + all_fail ))\n\n"
 XML="<testsuites tests=\"$(( all_pass + all_fail ))\" failures=\"${all_fail}\">\n$SUITES_XML</testsuites>\n"
